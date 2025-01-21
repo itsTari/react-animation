3f3c477256772f42fb56f6fb61b8fedf4d5ed622
itsTari <tariebisamuel16@gmail.com>
2025-01-21 21:30:38 +0100

    added new component

--- src/components/Lights.jsx
+++ src/components/Lights.jsx
@@ -2,18 +2,9 @@ import { Environment, Lightformer } from "@react-three/drei";
 
 const Lights = () => {
   return (
-    // group different lights and lightformers. We can use group to organize lights, cameras, meshes, and other objects in the scene.
     <group name="lights">
-      {/**
-       * @description Environment is used to create a background environment for the scene
-       * https://github.com/pmndrs/drei?tab=readme-ov-file#environment
-       */}
       <Environment resolution={256}>
         <group>
-          {/**
-           * @description Lightformer used to create custom lights with various shapes and properties in a 3D scene.
-           * https://github.com/pmndrs/drei?tab=readme-ov-file#lightformer
-           */}
           <Lightformer
             form="rect"
             intensity={10}
@@ -37,12 +28,6 @@ const Lights = () => {
           />
         </group>
       </Environment>
-
-      {/**
-       * @description spotLight is used to create a light source positioned at a specific point
-       * in the scene that emits light in a specific direction.
-       * https://threejs.org/docs/#api/en/lights/SpotLight
-       */}
       <spotLight
         position={[-2, 10, 5]}
         angle={0.15}
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,23 +1,46 @@
-import React, { Suspense } from 'react'
-import { PerspectiveCamera, View } from '@react-three/drei'
-import Lights from './Lights'
-import Iphone from './Iphone'
-
-const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, item, size}) => {
+import React, { Suspense} from "react";
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./Lights";
+import Iphone from "./Iphone";
+import * as THREE from 'three'
+import Loader from "./Loader";
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  item,
+  size,
+}) => {
+  
   return (
-    <View index={index} id={gsapType} className={`w-full h-full ${index === 2} ? 'right-[-100%] : '`}>
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full ${index === 2} ? 'right-[-100%] : '`}
+    >
       {/* ambient light */}
-      <ambientLight/>
-      <PerspectiveCamera makeDefault position={[0, 0, 4]}/>
+      <ambientLight />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
-      <Lights/>
-      <OrbitControls/>
-      <group ref={groupRef} name={`${index === 1} ? 'small' : 'Large' `}>
-          
+      <Lights />
+
+      <OrbitControls makeDefault ref={controlRef} enableZoom={false} enablePan={false} rotateSpeed={0.4} target={new THREE.Vector3(0, 0, 0)} onEnd={()=> setRotationState(controlRef.current.getAzimuthalAngle())}/>
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'Large'`}
+        position={[0, 0, 0]}
+      >
+        <Suspense
+          fallback={<Loader/>}
+          >
+          <Iphone scale={index === 1 ? [15, 15, 15] : [17, 17, 17]} item={item} size={size}/>
+        </Suspense>
       </group>
-     
     </View>
-  )
-}
+  );
+};
 
-export default ModelView
+export default ModelView;
