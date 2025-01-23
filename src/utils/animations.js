28fea0ecac66a6e887639e12a13a688346a7fabc
itsTari <tariebisamuel16@gmail.com>
2025-01-23 13:35:08 +0100

    complete the features component

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,4 +1,17 @@
-
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
+gsap.registerPlugin(ScrollTrigger);
+export const animateWithGsap = (target, animationProps, scrollProps) =>{
+    gsap.to(target, {
+        ...animationProps,
+        scrollTrigger:{
+            trigger:target,
+            toogleActions:'restart reverse restart reverse',
+            start:'top 85%',
+            ...scrollProps
+        }
+    })
+}
 export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps)=>{
         timeline.to(rotationRef.current.rotation , {
             y: rotationState,
