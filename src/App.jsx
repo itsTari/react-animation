ed237abcef8e1925b9e0aa072693065e2f766d84
itsTari <tariebisamuel16@gmail.com git config --global user.name>
2025-01-26 11:51:36 -0600

    modified the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Highlights from "./components/Highlights"
 import Model from "./components/Model"
 import Features from "./components/Features"
 import Chip from './components/Chip'
+import Footer from './components/Footer'
 const App =() =>{
   return (
     <>
@@ -14,6 +15,7 @@ const App =() =>{
         <Model/>
         <Features/>
         <Chip/>
+        <Footer/>
       </main>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-5 sm:px-10 px-5'>
+        <div className='screen-max-width '>
+            <div>
+                <p>More ways to shop:</p>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
