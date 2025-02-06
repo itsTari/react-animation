728e2087e0436c01a454fd2f7b96398f1e0198c5
itsTari <tariebisamuel16@gmail.com>
2025-02-06 13:46:11 +0100

    modified footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import {footerLinks} from '../constants'
 
 const Footer = () => {
   return (
-    <footer className='py-5 sm:px-10 px-5'>
+    <footer className='py-5 sm:px-10 px-6'>
         <div className='screen-max-width '>
             <div>
                 <p className='text-gray font-bold'>More ways to shop:
@@ -12,13 +12,13 @@ const Footer = () => {
                 <span className='underline text-blue'> +234 81076654 </span> {''} or <span className='underline text-blue'>+234 90632136</span> </p>
             </div>
             <div className='bg-neutral-700 my-5 h-[2px] w-full '/>
-            <div className='flex md:flex-row  md:items-center justify-between'>
-              <p className='font-semibold text-grey text-xs w-[50%] sm:w-[25%]'>copyright @2025 apple inc. all right reserved </p>
-              <div className='flex flex-col sm:flex-row md:flex-row'>
+            <div className='flex flex-col items-center md:flex-row gap-4  justify-between'>
+              <p className='font-semibold text-grey text-xs'>&copy; @{new Date().getFullYear()} apple inc. all right reserved </p>
+              <div className='flex justify-center  '>
                   {footerLinks.map((item)=>(
-                    // <div key={item.id} className='flex flex-col'>
-                        <li className='list-none text-gray font-bold md:border-r sm:border-r border-gray md:mx-1 lg:mx-2 md:px-1' > {item}</li>
-                    // </div>
+                    <div key={item.id} className='flex'>
+                        <li className='list-none text-gray text-xs  md:text-[16px] md:font-bold  border-r border-gray  lg:mx-2 px-1' > {item}</li>
+                    </div>
                     
                   ))}
               </div>
